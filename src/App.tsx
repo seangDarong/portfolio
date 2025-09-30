b98688c4083274a63b7fd2114419da55aa099c34
rong <darong6126@gmail.com>
2025-10-01 00:08:05 +0700

    fix: frost glass effect make it more glossy and align it better

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,14 @@
 import Navbar from './components/Navigation/Navbar.tsx'
 import About from './components/About/About.tsx'
 import './App.css'
+import Project from './components/Project/Projects.tsx'
 
 function App() {
   return (
     <>
       <Navbar/>
       <About/>
+      <Project/>
     </>
   )
 }
---
+++ src/components/Project/Projects.tsx
@@ -0,0 +1,13 @@
+    import React from 'react';
+    import './Project.css';
+    import profile from '../../assets/profile.png';
+
+    const About: React.FC = () => {
+    return (
+        <section id="about" className="about-section">
+        <div className='space'></div>
+        </section>
+    );
+    };
+
+    export default About;
